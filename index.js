01053eda6beb0ee5c4798e42d7b9e2952c5372cf
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-16 09:47:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,10 +24,9 @@ function viewCart() {
     return "Your shopping cart is empty."
   } else {
     for (let i = 0, len = cart.length; i < len; i++) {
-      cart[i]
-    } 
-    
-    string +=
+      string += cart[i]
+    }
+        
     bananas at $17, pancake batter at $5, and eggs at $49.
     return `In your cart, you have${string}`
   }
