22053bd9ffc858cef054f2babcbf6bb86eaf3578
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-16 09:15:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function addToCart(item) {
    itemName: `${item}`,
    itemPrice: `${Math.floor(Math.random() * 100 + 1)}`
  })
- return `${cart.slice(cart.length - 1)}`
+ return `${cart.slice(cart.length - 1)} has been added to your cart.`
 }
 
 function viewCart() {
