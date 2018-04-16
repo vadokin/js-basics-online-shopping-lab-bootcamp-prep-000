bac79761117cccfe4c460820d8d11aa69e280f45
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-16 09:49:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
     return "Your shopping cart is empty."
   } else {
     for (let i = 0, len = cart.length; i < len; i++) {
-      string += `cart[i] at \$${itemPrice}`
+      string += `${cart[i].itemName} at \$${itemPrice}`
     }
 
     bananas at $17, pancake batter at $5, and eggs at $49.
