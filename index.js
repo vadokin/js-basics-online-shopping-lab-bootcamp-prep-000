653c4571663a22592704f36eb67f2a32c265214a
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-16 13:28:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     string = `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
-    return
+    cart.length = 0
+    return string
   }
 }
