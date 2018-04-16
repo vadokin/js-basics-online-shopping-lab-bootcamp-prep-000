b150e1e2a13fe2f0355b2af61d28abbdedfba2f5
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-16 12:19:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function viewCart() {
 
 function total() {
   let sum = 0;
-  for (var i = 0, len = cart.length; i++, i < len; i++) {
+  for (var i = 0, len = cart.length; i < len; i++) {
     sum += cart[i].itemPrice
   }
   return sum
