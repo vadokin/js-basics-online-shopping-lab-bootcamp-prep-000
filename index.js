6d30e930a8d92f9f396f42969ccdf7cd6a28a8ac
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-16 13:21:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (true) {
-    
+  if (!cardNumber) {
+    return "Sorry, we don't have a credit card on file for you."
   }
 }
